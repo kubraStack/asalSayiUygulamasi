77f9846928730c4f2f9764f40c690fc2d9b6dbf1
Kübra Satılmış <kmrt3014@gmail.com>
2023-09-30 11:47:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace asalSayıUygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uygulama = Asal Sayı
+            // Girilen bir sayının asal olup olmadığını kontrol ediniz.
+            // Bir sayı sadece kendisine ve 1 'e bölünüyorsa asal sayıdır.
+
+           
+            bool asalmi = true;
+            System.Console.Write("Sayı: ");
+            int sayı = int.Parse(Console.ReadLine());
+
+            if (sayı == 1)
+            {
+               asalmi = false;
+            }
+            for (int i = 2; i < sayı; i++)
+            {
+                if (sayı%i == 0)
+                {
+                    asalmi = false;
+                    break;
+                }
+            }
+
+            if (asalmi)
+            {
+                System.Console.WriteLine("Sayı asal sayıdır");
+            }else{
+                System.Console.WriteLine("Sayı asal değildir");
+            }
+        }
+    }
+}
